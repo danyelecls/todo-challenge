a632b3a3f43dcd492a1263e1ba7ddf024601b9c4
nia <danyelecls@gmail.com>
2021-12-10 15:16:08 -0300

    Atualização serializers

--- todos/serializers.py
+++ todos/serializers.py
@@ -3,9 +3,13 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-
 class TodoSerializer(BaseModel):
     id: str
+    title: str
+    description: str
+    status: Status
+    responsible: str
+    due_date: datetime
 
     class Config:
         orm_mode = True
@@ -13,6 +17,11 @@ class TodoSerializer(BaseModel):
 
 class TodoCreateSerializer(BaseModel):
     title: str
+    description: str
+    status: Status
+    responsible: str
+    due_date: datetime
+
 
 class TodoUpdateSerializer(TodoCreateSerializer):
     pass
